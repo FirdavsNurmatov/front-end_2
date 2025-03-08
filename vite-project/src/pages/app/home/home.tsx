04d8c11736479ed2bc307232d1b2cda559280a15
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-03-08 16:51:40 +0500

    home page completed

--- vite-project/src/pages/app/home/home.tsx
+++ vite-project/src/pages/app/home/home.tsx
@@ -1,31 +1,146 @@
 import hideIcon from "../../../assets/svg/home/hide-icon.svg";
 import { useGetSingleStore } from "./service/query/useGetSingleStore";
+import walletIcon from "../../../assets/svg/home/wallet-icon.svg";
+import addBtnIcon from "../../../assets/svg/home/add-btn-icon.svg";
+import { useNavigate } from "react-router-dom";
+import calendarIcon from "../../../assets/svg/home/calendar-icon.svg";
+import { useState } from "react";
 
 export const Home = () => {
-  const count = 0;
+  const navigate = useNavigate();
+  const isPaid = true;
+  let storeFullName = "";
+  let storeAllDebtors = "";
+  let updatedStoreAllDebts = "";
+  let updatedStoreWallet = "";
 
   const { data: storeData, isLoading } = useGetSingleStore();
-  const storeFullName = storeData?.data?.fullname;
+  if (!isLoading) {
+    const { justStoreData, additionalStoreData } = storeData;
+
+    storeFullName = justStoreData?.data?.fullname;
+    storeAllDebtors = additionalStoreData?.data?.debtors_count;
+    const storeWallet = justStoreData?.data?.wallet;
+
+    const storeAllDebts = String(additionalStoreData?.data?.total_debts);
+    const storeAllDebtsLength = storeAllDebts.length - 7;
+    if (storeAllDebts.length > 6) {
+      for (let i = storeAllDebtsLength; i >= 0; i--) {
+        updatedStoreAllDebts = storeAllDebts[i] + updatedStoreAllDebts;
+        if (i % 3 == 0) {
+          updatedStoreAllDebts = " " + updatedStoreAllDebts;
+        }
+      }
+    } else {
+      for (let i = storeAllDebtsLength; i > 0; i--) {
+        updatedStoreAllDebts = storeAllDebts[i] + updatedStoreAllDebts;
+        if ((i + 1) % 3 == 0) {
+          updatedStoreAllDebts = " " + updatedStoreAllDebts;
+        }
+      }
+    }
+    updatedStoreAllDebts = updatedStoreAllDebts + " " + storeAllDebts.slice(-6);
+
+    let storeWalletLength = storeWallet.length - 7;
+    if (storeWallet.length > 6) {
+      for (let i = storeWalletLength; i >= 0; i--) {
+        updatedStoreWallet = storeWallet[i] + updatedStoreWallet;
+        if (i % 3 == 0) {
+          updatedStoreWallet = " " + updatedStoreWallet;
+        }
+      }
+    } else {
+      for (let i = storeWalletLength; i > 0; i--) {
+        updatedStoreWallet = storeWallet[i] + updatedStoreWallet;
+        if ((i + 1) % 3 == 0) {
+          updatedStoreWallet = " " + updatedStoreWallet;
+        }
+      }
+    }
+    updatedStoreWallet = updatedStoreWallet + " " + storeWallet.slice(-6);
+  }
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const goToStorePayment = () => {
+    navigate("/");
+  };
+
+  const goToStoreCalendar = () => {
+    navigate("/app/calendar");
+  };
 
   return (
     <div className=".container">
       <div className="home">
-        <div>
-          <img src="" alt="profile_image" />
-          <p>{isLoading ? "something went wrong" : storeFullName} </p>
+        <div className="store__hero">
+          <div className="store__img_name">
+            <img src="" alt="profile_image" />
+            <p className="store__fullname">
+              {isLoading ? "Loading..." : storeFullName}
+            </p>
+          </div>
+          <button className="store__calendar" onClick={goToStoreCalendar}>
+            <img className="home__icon" src={calendarIcon} alt="icon" />
+          </button>
         </div>
         <div className="all__summ">
           <div className="all__summ__block">
             <p className="all__summ__title">Umumiy nasiya:</p>
-            <p>{count} so'm</p>
+            <p className="all__summ__text">
+              {isLoading
+                ? "Loading..."
+                : isVisible
+                ? updatedStoreAllDebts
+                : "* * *"}{" "}
+              so'm
+            </p>
           </div>
-          <button className="all__summ__hide_icon">
-            <img src={hideIcon} alt="icon" />
+          <button
+            onClick={() => setIsVisible(!isVisible)}
+            className="all__summ__hide_icon"
+          >
+            <img className="eye_hide__icon" src={hideIcon} alt="icon" />
           </button>
         </div>
-        <div>
-          <div></div>
-          <div></div>
+        <div className="about__debtors">
+          <div className="about__debtors__summ">
+            <p className="about__debtors__summ__text">
+              Kechiktirilgan to‘lovlar
+            </p>
+            <p className="about__debtors__summ__quantity">{0}</p>
+          </div>
+          <div className="about__debtors__count">
+            <p className="about__debtors__count__text">Mijozlar soni</p>
+            <p className="about__debtors__count__quantity">
+              {storeAllDebtors ? storeAllDebtors : 0}
+            </p>
+          </div>
+        </div>
+        <div className="wallet">
+          <p className="wallet__title">Hamyoningiz</p>
+          <div className="wallet__title_block">
+            <img className="home__icon" src={walletIcon} alt="icon" />
+            <div className="your__wallet">
+              <div>
+                <p className="in_your__wallet">Hisobingizda</p>
+                <p className="in_your__wallet__summ">
+                  {isLoading ? "Loading..." : updatedStoreWallet} so'm
+                </p>
+              </div>
+              <button className="wallet__btn" onClick={goToStorePayment}>
+                <img className="home__icon" src={addBtnIcon} alt="icon" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="store__payment">
+          <p className="store__payment__title">Bu oy uchun to'lov:</p>
+          {isPaid ? (
+            <p className="store__payment__true">To'lov qilingan</p>
+          ) : (
+            <p className="store__payment__false">To'lov qilinmagan</p>
+          )}
         </div>
       </div>
     </div>
--- vite-project/src/router/app.routes.ts
+++ vite-project/src/router/app.routes.ts
@@ -2,9 +2,11 @@ import { Product } from "../pages/app/product/product";
 import { Debtors } from "../pages/app/debtors/debtors";
 import { Home } from "../pages/app/home/home";
 import { Report } from "../pages/app/report/report";
+import { Calendar } from "../pages/app/calendar/calendar";
 
 export const appRoutes = [
   { comp: Home, path: "home" },
+  { comp: Calendar, path: "calendar" },
   { comp: Debtors, path: "debtors" },
   { comp: Product, path: "product" },
   { comp: Report, path: "report" },
